ecb6c560b684fde1d0be7aaf7850d23f6d6e2161
Mussawer.Raza-at-101380220521 <mussawer@gosaas.io>
2021-11-26 18:56:35 +0500

    main component

---
+++ src/componenets/MainComponent/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from "@material-ui/core";
+import useStyles from "./styles";
+import Form from "./Form";
+import List from "./List";
+const MainComponent = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: "1.5em", marginTop: "20px" }}>
+          {/* <InfoCard/> */}
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MainComponent;
